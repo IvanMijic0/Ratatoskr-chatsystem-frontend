0f45d18f34ee1c90b36962f7f3ba090be1fb9221
Ivan Mijić <ivan.mijic@stu.ibu.edu.ba>
2023-11-02 00:21:02 +0100

    Added some packages.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { StyledEngineProvider } from "@mui/material";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<StyledEngineProvider injectFirst>
+		<App/>
+	</StyledEngineProvider>
+);
