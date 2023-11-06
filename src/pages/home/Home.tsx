38ddd4ee5c2be642b9d9e3dc8ce69cdf43b55e80
Ivan Mijić <ivan.mijic@stu.ibu.edu.ba>
2023-11-06 03:41:01 +0100

    Playing with Grid.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,22 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 import BottomNavigationBar from "../../components/bottom_navigation/BottomNavigationBar.tsx";
 import ratatoskrImage from "../../assets/ratatoskr.png";
+import runeTitle from "../../assets/runes.svg";
 import classes from "./Home.module.css";
+import { Box } from "@mui/material";
 
 const Home = () => {
-	return <Grid container spacing={ 12 }>
-		<img className={ classes.background } src={ ratatoskrImage } alt="Ratatoskr image"/>
+	return <Box sx={ { flexGrow: 1 } }>
+		<Grid container spacing={ 72 } columns={ { xs: 4, sm: 8, md: 12 } }>
+			<Grid xs>
+				<img className={ classes.background } src={ ratatoskrImage } alt="Ratatoskr image"/>
+			</Grid>
+			<Grid xs>
+				<img className={ classes.runeTitle } src={ runeTitle } alt="Rune title"/>
+			</Grid>
+		</Grid>;
 		<BottomNavigationBar/>
-	</Grid>;
+	</Box>;
 };
 
 export default Home;
