97c3bec9f163dbf7fcc1eca2f93825e9b58010d6
Andrey Baranov <baranov.a.64103@gmail.com>
2024-01-02 21:43:50 +0100

    NFTStore layout and web3 start

--- src/pages/NFTStore/NFTStore.tsx
+++ src/pages/NFTStore/NFTStore.tsx
@@ -1,11 +1,33 @@
-import { Box, Typography } from "@mui/material";
-
+import { useEffect, useState } from 'react';
+import { Box, Grid } from "@mui/material";
 import classes from './NFTStore.module.css';
+import NFTStoreCard from "./NFTStoreCard.tsx";
+import sampleNFTs from "./sampleNFTs.ts";
 
-const NFTStore = () => {
-	return <Box className={ classes.background }>
-		<Typography variant="h1">NFT STORE</Typography>
-	</Box>;
-};
+// import Web3 from 'web3';
+// import NFTContractABI from './NFTContractABI.json';
+// const web3 = new Web3('');
+// const nftContract = new web3.eth.Contract(NFTContractABI, '');
+
+
+function NFTStore() {
+	const [NFT, setNFT] = useState<NonNullable<unknown>[]>([]);
+
+	useEffect(() => {
+		setNFT(sampleNFTs);
+	}, []);
+
+	return (
+		<Box className={classes.background} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+			<Grid container spacing={3} sx={{ textAlign: 'center' }}>
+				{NFT.map((nft, index) => (
+					<Grid item xs={12} sm={6} md={4} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+						<NFTStoreCard nft={nft} />
+					</Grid>
+				))}
+			</Grid>
+		</Box>
+	);
+}
 
 export default NFTStore;
---
+++ src/pages/NFTStore/NFTStoreCard.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+    onClick: () => void;
+    'aria-expanded': boolean;
+}
+
+function ExpandMore(props: ExpandMoreProps) {
+    const { expand, onClick, 'aria-expanded': ariaExpanded } = props;
+    return (
+        <IconButton
+            onClick={onClick}
+            aria-expanded={ariaExpanded}
+            aria-label="show more"
+        >
+            <ExpandMoreIcon transform={expand ? 'rotate(180deg)' : undefined} />
+        </IconButton>
+    );
+}
+
+interface NFTStoreCardProps {
+    nft: {
+        name: string;
+        description: string;
+        image: string;
+        attributes: {
+            trait_type: string;
+            value: string;
+        }[];
+    };
+}
+
+function NFTStoreCard({ nft }: NFTStoreCardProps) {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="nft">
+                        {nft.name[0]}
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={nft.name}
+                subheader={`Generation: ${nft.attributes.find(
+                    (attr) => attr.trait_type === 'Generation'
+                )?.value}`}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={nft.image}
+                alt={nft.name}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {nft.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                />
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Attributes:</Typography>
+                    <ul>
+                        {nft.attributes.map((attr, index) => (
+                            <li key={index}>
+                                <strong>{attr.trait_type}:</strong> {attr.value}
+                            </li>
+                        ))}
+                    </ul>
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
+
+export default NFTStoreCard;
