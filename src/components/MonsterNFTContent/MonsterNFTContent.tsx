b6bfb1c468d72e2755ae8af3dc2636c173223d1b
Ivan Mijić <ivan.mijic@stu.ibu.edu.ba>
2024-01-12 12:43:59 +0100

    Refactored DescriptionPane.tsx

--- src/components/MonsterNFTContent/MonsterNFTContent.tsx
+++ src/components/MonsterNFTContent/MonsterNFTContent.tsx
@@ -29,63 +29,60 @@ export const MonsterNFTContent = ( { monsterData }: { monsterData: any } ) => {
 		);
 	};
 
-	return (
-		<Box>
+	return <Box>
+		{ monsterData.length > 0 && (
+			<Box className={ classes.description }>
 
-			{ monsterData.length > 0 && (
-				<Box className={ classes.description }>
-
-					<CustomTooltip
-						title={ monsterData[currentIndex].description }
-						placement="top">
-						{ monsterData[currentIndex].image && (
-							<img
-								className={ classes.image }
-								src={ convertIpfsUriToUrl(monsterData[currentIndex].image) }
-								alt={ `monster-${ currentIndex }` }
-							/>
-						) }
-					</CustomTooltip>
-					<Box className={ classes.buttons }>
-						<IconButton className={ classes.button } onClick={ handlePrev }>
-							<NavigateBeforeIcon/>
-						</IconButton>
-						<IconButton className={ classes.button } onClick={ handleNext }>
-							<NavigateNextIcon/>
-						</IconButton>
-					</Box>
-
-					<Box className={ classes.content }>
-						<Typography>{ monsterData[currentIndex].name }</Typography>
-
-						<Typography
-							className={ classes.desc }>{ monsterData[currentIndex].description }</Typography>
-
-						{ monsterData[currentIndex].attributes && (
-							<ul>
-								{ monsterData[currentIndex].attributes.type && (
-									<li>Type: { monsterData[currentIndex].attributes.type }</li>
-								) }
-								{ monsterData[currentIndex].attributes.element && (
-									<li>Element: { monsterData[currentIndex].attributes.element }</li>
-								) }
-								{ monsterData[currentIndex].attributes.rarity && (
-									<li>Rarity: { monsterData[currentIndex].attributes.rarity }</li>
-								) }
-								{ monsterData[currentIndex].attributes.level && (
-									<li>Level: { monsterData[currentIndex].attributes.level }</li>
-								) }
-								<li>HP: { monsterData[currentIndex].attributes.hp }</li>
-								<li>Attack: { monsterData[currentIndex].attributes.attack }</li>
-								<li>Defense: { monsterData[currentIndex].attributes.defense }</li>
-								<li>Generation: { monsterData[currentIndex].attributes.generation }</li>
-							</ul>
-						) }
-					</Box>
+				<CustomTooltip
+					title={ monsterData[currentIndex].description }
+					placement="top">
+					{ monsterData[currentIndex].image && (
+						<img
+							className={ classes.image }
+							src={ convertIpfsUriToUrl(monsterData[currentIndex].image) }
+							alt={ `monster-${ currentIndex }` }
+						/>
+					) }
+				</CustomTooltip>
+				<Box className={ classes.buttons }>
+					<IconButton className={ classes.button } onClick={ handlePrev }>
+						<NavigateBeforeIcon/>
+					</IconButton>
+					<IconButton className={ classes.button } onClick={ handleNext }>
+						<NavigateNextIcon/>
+					</IconButton>
 				</Box>
-			) }
-		</Box>
-	);
+
+				<Box className={ classes.content }>
+					<Typography>{ monsterData[currentIndex].name }</Typography>
+
+					<Typography
+						className={ classes.desc }>{ monsterData[currentIndex].description }</Typography>
+
+					{ monsterData[currentIndex].attributes && (
+						<ul>
+							{ monsterData[currentIndex].attributes.type && (
+								<li>Type: { monsterData[currentIndex].attributes.type }</li>
+							) }
+							{ monsterData[currentIndex].attributes.element && (
+								<li>Element: { monsterData[currentIndex].attributes.element }</li>
+							) }
+							{ monsterData[currentIndex].attributes.rarity && (
+								<li>Rarity: { monsterData[currentIndex].attributes.rarity }</li>
+							) }
+							{ monsterData[currentIndex].attributes.level && (
+								<li>Level: { monsterData[currentIndex].attributes.level }</li>
+							) }
+							<li>HP: { monsterData[currentIndex].attributes.hp }</li>
+							<li>Attack: { monsterData[currentIndex].attributes.attack }</li>
+							<li>Defense: { monsterData[currentIndex].attributes.defense }</li>
+							<li>Generation: { monsterData[currentIndex].attributes.generation }</li>
+						</ul>
+					) }
+				</Box>
+			</Box>
+		) }
+	</Box>;
 };
 
 export default MonsterNFTContent;
